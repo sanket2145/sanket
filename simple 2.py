0591965f1687e45e72eee2d97afb9dba9fd2a443
sanket2145 <sanketingle01@gmail.com>
2025-04-22 19:14:11 +0530

    Add files via upload

---
+++ simple 2.py
@@ -0,0 +1,8 @@
+a=5+10
+print(a)
+a=5-10
+print(a)
+a=5*10
+print(a)
+a=5/10
+print(a)
---
+++ simple program.py
@@ -0,0 +1,4 @@
+a=input("first name")
+b=input("last name")
+c= a + b
+print("hello, " +c, "welcome to python program")
